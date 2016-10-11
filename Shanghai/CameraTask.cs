26bcb0145e811ff80752f2b99b0cd3d6c903c76b
yappy <superunyo@yahoo.co.jp>
2016-10-11 21:57:13 +0900

    サムネイル作成

--- Shanghai/CameraTask.cs
+++ Shanghai/CameraTask.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace Shanghai
 {
@@ -14,6 +16,13 @@ namespace Shanghai
         private static readonly string WebDir = "www";
         private static readonly string PicDir = Path.Combine(WebDir, "pics");
         private static readonly string TweetDir = Path.Combine(WebDir, "twque");
+
+        // x:y:quality
+        private static readonly int ThumbX = 160;
+        private static readonly int ThumbY = 120;
+        private static readonly int ThumbQuality = 100;
+        private static readonly string ThumOption = string.Format(
+            "{0}:{1}:{2}", ThumbX, ThumbY, ThumbQuality);
 
         private static CameraSettings settings
         {
@@ -35,14 +44,21 @@ namespace Shanghai
             string dirPath = Path.Combine(PicDir, dirName);
             string fileName = now.ToString("yyyyMMdd_HHmm") + ".jpg";
             string filePath = Path.Combine(PicDir, dirName, fileName);
+            string thName = now.ToString("yyyyMMdd_HHmm") + "_th.jpg";
+            string thPath = Path.Combine(PicDir, dirName, thName);
 
             Log.Trace.TraceEvent(TraceEventType.Information, 0,
                 "Take a picture: {0}", filePath);
 
             Directory.CreateDirectory(dirPath);
             var p = Process.Start("raspistill",
-                string.Format(@"-t 1 -o ""{0}""", filePath));
+                string.Format(@"-o ""{0}"" -thumb {1}", filePath, ThumOption));
             p.WaitForExit();
+
+            // Create thumb
+            var image = Image.FromFile(filePath);
+            var thumb = image.GetThumbnailImage(ThumbX, ThumbY, null, IntPtr.Zero);
+            thumb.Save(thPath, ImageFormat.Jpeg);
         }
     }
 }
