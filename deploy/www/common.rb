57a034ca5546d5e508cbc338e9684b1917027f7c
yappy <superunyo@yahoo.co.jp>
2016-10-11 22:22:14 +0900

    CGI 修正

--- deploy/www/common.rb
+++ deploy/www/common.rb
@@ -2,6 +2,6 @@ module Const
 	PIC_DIRS = "pics/*".freeze
 	PIC_PATTERN = /pics\/(\d+)/.freeze
 	TH_FILES = "pics/%s/*_th.jpg".freeze
-	TH_PATTERN = "pics/.*/(\d+)_th.jpg".freeze
+	TH_PATTERN = /pics\/.*\/(\d+_\d+)_th.jpg/.freeze
 	PIC_FILE = "pics/%s/%s.jpg".freeze
 end
