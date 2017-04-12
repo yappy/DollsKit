3a5e9380d52eef70aba325228e1c531f0d6ad43c
yappy <superunyo@yahoo.co.jp>
2017-04-12 21:40:08 +0900

    テスト用アプリケーション追加

--- DollsLang/Lexer.cs
+++ DollsLang/Lexer.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace DollsLang
 {
-    class Lexer
+    public class Lexer
     {
         private struct Target
         {
@@ -141,59 +139,6 @@ namespace DollsLang
                 default:
                     return orgType;
             }
-        }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var lexer = new Lexer();
-
-            string test1 = "z = 1 / 0 t = 3 s = 3.14 u = 1e10\n";
-            test1 += "x = 5 + t * -2 \n";
-            test1 += "s = \"this is string\" \n";
-            test1 += "p(nil) p(false) p(true)\n";
-            test1 += "print(t x s) \n";
-            test1 += "if(x>0){print(1)}elif(x<-10){print(2)}else{print(3)}\n";
-            test1 += "i=5 while(i>0){print(i) i=i-1}\n";
-
-            StringBuilder test2 = new StringBuilder("a=");
-            const int depth = 100;
-            for (int i = 0; i < depth; i++)
-            {
-                test2.Append('(');
-            }
-            test2.Append('1');
-            for (int i = 0; i < depth; i++)
-            {
-                test2.Append(')');
-            }
-
-            var tokenList = lexer.Process(test1);
-            foreach (var token in tokenList)
-            {
-                Console.WriteLine(token);
-            }
-            Console.WriteLine();
-
-            var parser = new Parser();
-            var program = parser.Parse(tokenList);
-            var buf = new StringBuilder();
-            program.Print(buf, 0);
-            Console.WriteLine(buf);
-
-            var runtime = new Runtime();
-            runtime.LoadDefaultFunctions();
-            runtime.Execute(program);
-
-            tokenList = lexer.Process(test2.ToString());
-            program = parser.Parse(tokenList);
-            buf.Clear();
-            program.Print(buf, 0);
-            Console.WriteLine(buf);
-
-            Console.Read();
         }
     }
 }
--- DollsLang/Value.cs
+++ DollsLang/Value.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Text;
-
-namespace DollsLang
+﻿namespace DollsLang
 {
     public enum ValueType
     {
