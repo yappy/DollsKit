eed6bc8598ce2df66d4f8cf58316c55b383b46c6
yappy <superunyo@yahoo.co.jp>
2017-05-23 23:01:18 +0900

    draw 関数追加
    パラメータ表示で簡単にグラフを描く

--- DollsLang/RuntimeGraphLib.cs
+++ DollsLang/RuntimeGraphLib.cs
@@ -40,6 +40,7 @@ namespace DollsLang
         private void LoadGraphFunctionsInternal()
         {
             LoadFunction("line", LibLine);
+            LoadFunction("draw", LibDraw);
         }
 
         private Point Transform(double x, double y)
@@ -54,13 +55,48 @@ namespace DollsLang
 
         private Value LibLine(Value[] args)
         {
+            graphicsEnabled = true;
+
             double x1 = GetParam(args, 0).ToFloat();
             double y1 = GetParam(args, 1).ToFloat();
             double x2 = GetParam(args, 2).ToFloat();
             double y2 = GetParam(args, 3).ToFloat();
 
-            graphicsEnabled = true;
             g.DrawLine(pen, Transform(x1, y1), Transform(x2, y2));
+
+            return NilValue.Nil;
+        }
+
+        private Value LibDraw(Value[] args)
+        {
+            graphicsEnabled = true;
+
+            double init = GetParam(args, 0).ToFloat();
+            double end = GetParam(args, 1).ToFloat();
+            int count = GetParam(args, 2).ToInt();
+            FunctionValue func = GetParam(args, 3).ToFunction();
+            if (count < 1)
+            {
+                throw new RuntimeLangException($"Count is less than 1: {count}");
+            }
+
+            double dt = (end - init) / count;
+
+            Func<double, Point> call = (t) => {
+                ArrayValue result = CallFunction(func, new FloatValue(t)).ToArray();
+                double x = ReadArray(result, 0).ToFloat();
+                double y = ReadArray(result, 1).ToFloat();
+                return Transform(x, y);
+            };
+
+            Point prev = call(init);
+            for (int i = 1; i <= count; i++)
+            {
+                double t = init + dt * i;
+                Point cur = call(t);
+                g.DrawLine(pen, prev, cur);
+                prev = cur;
+            }
 
             return NilValue.Nil;
         }
