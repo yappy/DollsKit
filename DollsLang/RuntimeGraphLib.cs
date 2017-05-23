b9b0593295f514b4fddc7103c304a3ae2978ceb4
yappy <superunyo@yahoo.co.jp>
2017-05-23 20:43:36 +0900

    line 関数で線を引けるように

--- DollsLang/RuntimeGraphLib.cs
+++ DollsLang/RuntimeGraphLib.cs
@@ -7,10 +7,13 @@ namespace DollsLang
 {
     public partial class Runtime
     {
-        private static readonly int SIZE_W = 256;
-        private static readonly int SIZE_H = 256;
-        private Bitmap bitmap = new Bitmap(SIZE_W, SIZE_H);
+        private static readonly int SizeW = 256;
+        private static readonly int SizeH = 256;
+        private static readonly Pen DefaultPen = Pens.Black;
+
+        private Bitmap bitmap = new Bitmap(SizeW, SizeH);
         private Graphics g = null;
+        private Pen pen = DefaultPen;
         private bool graphicsEnabled = false;
 
         private Bitmap GetGraphicsResultInternal()
@@ -18,7 +21,7 @@ namespace DollsLang
             return graphicsEnabled ? bitmap : null;
         }
 
-        private void InitializeGraphRuntimeInternal()
+        private void InitializeGraphRuntime()
         {
             if (g != null)
             {
@@ -26,6 +29,7 @@ namespace DollsLang
             }
             g = Graphics.FromImage(bitmap);
             g.Clear(Color.White);
+            pen = DefaultPen;
         }
 
         private void LoadGraphVariablesInternal()
@@ -35,7 +39,30 @@ namespace DollsLang
 
         private void LoadGraphFunctionsInternal()
         {
-            //LoadFunction("print", LibPrint);
+            LoadFunction("line", LibLine);
+        }
+
+        private Point Transform(double x, double y)
+        {
+            // (SIZE / 2) + v * (SIZE / 2)
+            // = (SIZE / 2) * (1 + v)
+            // REVERSE y
+            return new Point(
+                (int)(SizeW / 2 * (1.0 + x)),
+                (int)(SizeH / 2 * (1.0 - y)));
+        }
+
+        private Value LibLine(Value[] args)
+        {
+            double x1 = GetParam(args, 0).ToFloat();
+            double y1 = GetParam(args, 1).ToFloat();
+            double x2 = GetParam(args, 2).ToFloat();
+            double y2 = GetParam(args, 3).ToFloat();
+
+            graphicsEnabled = true;
+            g.DrawLine(pen, Transform(x1, y1), Transform(x2, y2));
+
+            return NilValue.Nil;
         }
     }
 }
