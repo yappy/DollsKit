bcd0ecb8a46f29bdf20bcff10c8e3dedf361825a
yappy <superunyo@yahoo.co.jp>
2017-05-23 19:39:35 +0900

    グラフィクスライブラリ追加
    実行結果にイメージ出力を追加

---
+++ DollsLang/RuntimeGraphLib.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace DollsLang
+{
+    public partial class Runtime
+    {
+        private static readonly int SIZE_W = 256;
+        private static readonly int SIZE_H = 256;
+        private Bitmap bitmap = new Bitmap(SIZE_W, SIZE_H);
+        private Graphics g = null;
+        private bool graphicsEnabled = false;
+
+        private Bitmap GetGraphicsResultInternal()
+        {
+            return graphicsEnabled ? bitmap : null;
+        }
+
+        private void InitializeGraphRuntimeInternal()
+        {
+            if (g != null)
+            {
+                g.Dispose();
+            }
+            g = Graphics.FromImage(bitmap);
+            g.Clear(Color.White);
+        }
+
+        private void LoadGraphVariablesInternal()
+        {
+            
+        }
+
+        private void LoadGraphFunctionsInternal()
+        {
+            //LoadFunction("print", LibPrint);
+        }
+    }
+}
