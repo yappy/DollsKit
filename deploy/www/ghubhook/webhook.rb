7bb295261d52fa1673f4299a3dd7a367fc1b8055
yappy <superunyo@yahoo.co.jp>
2017-11-15 21:24:39 +0900

    webhook の署名確認

--- deploy/www/ghubhook/webhook.rb
+++ deploy/www/ghubhook/webhook.rb
@@ -1,17 +1,35 @@
+require 'tempfile'
 require 'cgi'
 require 'json'
 require 'mysql2'
+require 'openssl'
 load 'common.rb'
 
+@config = JSON.parse(File.read("../config.json"))
+
+# copy stdin to file
+unless File.file?($stdin)
+	temp = Tempfile.open('stdin') do |temp|
+		$stdin.binmode
+		IO.copy_stream($stdin, temp)
+		temp
+	end
+	$stdin = temp.open
+	$stdin.binmode
+end
+# read for signiture check and rewind
+@payload_body = $stdin.read
+$stdin.rewind
+
+# parse $stdin if POST
 @cgi = CGI.new("html5")
 
 def github_push(payload)
-	config = JSON.parse(File.read("../config.json"))
 	client = Mysql2::Client.new(
 		:encoding => 'utf8mb4',
-		:username => config["user"],
-		:password => config["pass"],
-		:database => config["database"])
+		:username => @config["user"],
+		:password => @config["pass"],
+		:database => @config["database"])
 	statement = client.prepare("INSERT INTO #{Const::TABLE_NAME}" \
 		"(ref, hash_before, hash_after, compare, repo_fname, head_msg) " \
 		"VALUES (?, ?, ?, ?, ?, ?)")
@@ -22,6 +40,22 @@ def github_push(payload)
 		payload["compare"],
 		payload["repository"]["full_name"],
 		payload["head_commit"]["message"]);
+end
+
+def secure_compare(a, b)
+	return false unless a.bytesize == b.bytesize
+
+	l = a.unpack("C*")
+
+	r, i = 0, -1
+	b.each_byte { |v| r |= v ^ l[i+=1] }
+	r == 0
+end
+
+def verify_signature(payload_body, token, github_signature)
+	signature = 'sha1=' + OpenSSL::HMAC.hexdigest(
+		OpenSSL::Digest.new('sha1'), token, payload_body)
+	secure_compare(signature, github_signature)
 end
 
 def create_html(title, body)
@@ -41,6 +75,12 @@ end
 def post()
 	github_delivery = ENV["HTTP_X_GITHUB_DELIVERY"]
 	github_event = ENV["HTTP_X_GITHUB_EVENT"]
+	github_signature = ENV['HTTP_X_HUB_SIGNATURE']
+
+	unless verify_signature(@payload_body, @config["token"],
+		github_signature)
+		return ["SERVER_ERROR", "<P>Signature error</P>"]
+	end
 
 	case github_event
 	when "ping" then
