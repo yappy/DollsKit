ea57f2043c090da7526e34cd69e85b6bd0383ac4
yappy <superunyo@yahoo.co.jp>
2017-11-18 20:33:53 +0900

    管理対象のプロセスを起動, SIGTERM で stdin にコマンド送信

--- daemon/dollsd.rb
+++ daemon/dollsd.rb
@@ -7,7 +7,9 @@
 #
 
 require 'logger'
+require 'open3'
 
+EXEC_CMD = "ruby -e 'p $stdin.gets'"
 LOG_FILE = "dollsd.log".freeze
 
 $logger = nil
@@ -18,13 +20,23 @@ $args = {
 
 class DollsDaemon
 	def initialize
-		@sig_int
+		# signal flags
+		@sig_int = false
 		@sig_term = false
 		@sig_hup = false
+
+		# child process
+		@child = {
+			:wait_thr => nil,
+			:stdin    => nil,
+			:stdout   => nil,
+			:stderr   => nil,
+		};
 	end
 
 	def run
 		setup
+		exec_proc
 		main_loop
 	end
 
@@ -59,19 +71,71 @@ private
 		$logger.info "sigaction OK"
 	end
 
+	def exec_proc
+		raise "assert" if @child[:wait_thr]
+
+		stdin, stdout, stderr, wait_thr = *Open3::popen3(EXEC_CMD)
+		@child[:wait_thr] = wait_thr
+		@child[:stdin] = stdin
+		@child[:stdout] = stdout
+		@child[:stderr] = stderr
+	end
+
+	def on_exit_proc
+		exit_code = @child[:wait_thr].value
+		$logger.info "Dolls process exit (code=#{exit_code})"
+
+		@child[:stdin].close
+		@child[:stdout].close
+		@child[:stderr].close
+
+		@child.clear
+	end
+
 	def main_loop
 		loop do
+			# process signals
 			if @sig_int or @sig_term then
 				$logger.info "[SIGTERM]"
+
+				send_cmd("SHUTDOWN");
+
 				@sig_int = false
 				@sig_term = false
-				break
 			end
 			if @sig_hup then
 				$logger.info "[SIGHUP]"
+
+				# TODO: reload
+
 				@sig_hup = false
 			end
-			sleep 1
+			# wait for process exit
+			if @child[:wait_thr].join(1) then
+				on_exit_proc
+				break
+			end
+		end
+	end
+
+	def send_cmd(cmd)
+		$logger.info "send command: #{cmd}"
+		cmd = "\n" + cmd + "\n"
+		loop do
+			begin
+				expected = cmd.bytesize
+				actual = @child[:stdin].write_nonblock(cmd)
+				if actual != expected then
+					raise "write_nonblock #{expected} returns #{actual}"
+				end
+				return true
+			rescue Errno::EINTR
+				# do again
+			rescue => err
+				$logger.error "send command failed"
+				$logger.error err
+				return false
+			end
 		end
 	end
 end
