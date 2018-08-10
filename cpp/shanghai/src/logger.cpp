4a7006b9a229a178a333e6b0b49885a3090ede0c
yappy <superunyo@yahoo.co.jp>
2018-08-11 00:43:38 +0900

    タイムスタンプを文字列化して付与

--- cpp/shanghai/src/logger.cpp
+++ cpp/shanghai/src/logger.cpp
@@ -1,6 +1,7 @@
 #include "logger.h"
 #include <cstdarg>
 #include <cstdio>
+#include <ctime>
 
 namespace shanghai {
 
@@ -8,7 +9,8 @@ namespace {
 
 using mtx_guard = std::lock_guard<std::mutex>;
 
-const size_t MsgLenMax = 1024;
+const uint32_t MsgLenMax = 512;
+const uint32_t LogLenMax = 1024;
 
 // 標準出力の実装
 class StdOutTarget : public LogTarget {
@@ -27,22 +29,35 @@ public:
 
 void Logger::Log(LogLevel level, const char *fmt, ...) noexcept
 {
-	va_list arg;
-	char msg[MsgLenMax];
+	std::time_t timestamp = std::time(nullptr);
+	char msg[MsgLenMax] = "";
+	char timestr[64] = "";
+	char logstr[LogLenMax] = "";
 
-	va_start(arg, fmt);
-	vsnprintf(msg, sizeof(msg) - 1, fmt, arg);
-	msg[sizeof(msg) - 1] = '\0';
-	va_end(arg);
-
+	// msg <- sprintf(fmt, ...)
+	{
+		va_list arg;
+		va_start(arg, fmt);
+		std::vsnprintf(msg, sizeof(msg) - 1, fmt, arg);
+		msg[sizeof(msg) - 1] = '\0';
+		va_end(arg);
+	}
 	{
 		mtx_guard lock(m_mtx);
+
+		// ローカルタイムへの変換がスレッドセーフでないので一緒に排他する
+		struct tm *local = std::localtime(&timestamp);
+		if (std::strftime(timestr, sizeof(timestr), "%c", local) == 0) {
+			timestr[0] = '\0';
+		}
+
+		std::snprintf(logstr, sizeof(logstr) - 1, "%s: %s", timestr, msg);
+		logstr[sizeof(logstr) - 1] = '\0';
 
 		for (auto &target : m_targets) {
 			if (target->CheckLevel(level)) {
 				try {
-					// TODO
-					target->Write(msg);
+					target->Write(logstr);
 				}
 				catch (...) {
 					std::fprintf(stderr, "Error on log write\n");
