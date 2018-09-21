1a5f517492ceaad1f128f3fd0aac4d44ecc8e964
yappy <superunyo@yahoo.co.jp>
2018-09-21 21:46:34 +0900

    リクエスト処理をやや高レベルなものに整える

--- cpp/shanghai/src/system/httpserver.cpp
+++ cpp/shanghai/src/system/httpserver.cpp
@@ -15,6 +15,15 @@ void AtPanic(void *cls, const char *file, unsigned int line, const char *reason)
 	std::terminate();
 }
 
+int IterateToMap(void *cls, enum MHD_ValueKind kind,
+	const char *key, const char *value) noexcept
+{
+	auto &map = *static_cast<KeyValueSet *>(cls);
+	value = (value == nullptr) ? "" : value;
+	map.emplace(key, value);
+	return MHD_YES;
+}
+
 }	// namespace
 
 HttpServer::HttpServer() : m_daemon(nullptr)
@@ -29,7 +38,7 @@ HttpServer::HttpServer() : m_daemon(nullptr)
 	::MHD_set_panic_func(AtPanic, nullptr);
 	m_daemon = ::MHD_start_daemon(
 		MHD_USE_SELECT_INTERNALLY, port, nullptr, nullptr,
-		Answer, this,
+		OnRequest, this,
 		MHD_OPTION_CONNECTION_MEMORY_LIMIT, MemoryLimit,
 		MHD_OPTION_CONNECTION_LIMIT, MaxConn,
 		MHD_OPTION_CONNECTION_TIMEOUT, TimeoutSec,
@@ -50,22 +59,77 @@ HttpServer::~HttpServer()
 	m_daemon = nullptr;
 }
 
-int HttpServer::Answer(void *cls, struct MHD_Connection *connection,
+HttpResponse HttpServer::ProcessRequest(struct MHD_Connection *connection,
+	const std::string &url, const std::string &method,
+	const std::string &version, const char *upload_data,
+	size_t *upload_data_size, void **con_cls) noexcept
+{
+	logger.Log(LogLevel::Info, "[HTTP] %s %s %s",
+		version.c_str(), method.c_str(), url.c_str());
+
+	// version: "505 HTTP Version Not Supported"
+	if (version != "HTTP/1.1") {
+		return HttpResponse(505);
+	}
+
+	// method: GET, HEAD, POST 以外は "501 Not Implemented"
+	if (method != "GET" && method != "HEAD" && method != "POST") {
+		return HttpResponse(501);
+	}
+
+	// HTTP request header と query を map に変換する
+	KeyValueSet request_header;
+	KeyValueSet get_args;
+	::MHD_get_connection_values(connection, MHD_HEADER_KIND,
+		IterateToMap, &request_header);
+	::MHD_get_connection_values(connection, MHD_GET_ARGUMENT_KIND,
+		IterateToMap, &get_args);
+	for (const auto &entry : request_header) {
+		logger.Log(LogLevel::Trace, "%s: %s",
+			entry.first.c_str(), entry.second.c_str());
+	}
+	for (const auto &entry : get_args) {
+		logger.Log(LogLevel::Trace, "%s=%s",
+			entry.first.c_str(), entry.second.c_str());
+	}
+
+	return HttpResponse(200,
+		{{"Content-Type", "text/html; charset=utf-8"}},
+		"<!DOCTYPE html>\n<html><body>Hello</body></html>\n");
+}
+
+int HttpServer::OnRequest(void *cls, struct MHD_Connection *connection,
 	const char *url, const char *method,
 	const char *version, const char *upload_data,
 	size_t *upload_data_size, void **con_cls) noexcept
 {
 	auto self = static_cast<HttpServer *>(cls);
-	logger.Log(LogLevel::Info, "[HTTP] %s %s", method, url);
 
-	char resbuf[] = "Hello\n";
-	MHD_Response *response = MHD_create_response_from_buffer(
-		sizeof(resbuf) - 1, resbuf, MHD_RESPMEM_MUST_COPY);
-	if (response == nullptr) {
+	// non-static に移行
+	// HttpResponse オブジェクトを返してもらう
+	HttpResponse resp = self->ProcessRequest(
+		connection, url, method, version,
+		upload_data, upload_data_size, con_cls);
+
+	// HttpResponse を変換処理してクライアントに返す
+	// ソースを確認したが malloc してそこに memcpy しているだけなので
+	// const を外しても問題ない
+	auto resp_del = [](MHD_Response *r) {
+		::MHD_destroy_response(r);
+	};
+	MHD_Response *tmp = MHD_create_response_from_buffer(
+		resp.Body.size(), const_cast<char *>(resp.Body.c_str()),
+		MHD_RESPMEM_MUST_COPY);
+	std::unique_ptr<MHD_Response, decltype(resp_del)> mhd_resp(tmp, resp_del);
+	if (mhd_resp == nullptr) {
+		logger.Log(LogLevel::Error, "MHD_create_response_from_buffer failed");
 		return MHD_NO;
 	}
-	int ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
-	MHD_destroy_response(response);
+	int ret = MHD_queue_response(connection, resp.Status, mhd_resp.get());
+	if (ret != MHD_YES) {
+		logger.Log(LogLevel::Error, "MHD_queue_response failed");
+		return MHD_NO;
+	}
 
 	return MHD_YES;
 }
--- cpp/shanghai/src/system/httpserver.h
+++ cpp/shanghai/src/system/httpserver.h
@@ -1,14 +1,57 @@
+/*
+References: RFC HTTP spec
+https://tools.ietf.org/html/rfc7230
+https://tools.ietf.org/html/rfc7231
+https://tools.ietf.org/html/rfc7232
+https://tools.ietf.org/html/rfc7233
+https://tools.ietf.org/html/rfc7234
+https://tools.ietf.org/html/rfc7235
+*/
+
 #ifndef SHANGHAI_SYSTEM_HTTPSERVER_H
 #define SHANGHAI_SYSTEM_HTTPSERVER_H
 
 #include <stddef.h>
 #include <stdint.h>
+#include <unordered_map>
 
 struct MHD_Daemon;
 struct MHD_Connection;
 
 namespace shanghai {
 namespace system {
+
+using KeyValueSet = std::unordered_map<std::string, std::string>;
+
+struct HttpResponse {
+	uint32_t Status;
+	KeyValueSet Header;
+	std::string Body;
+
+	explicit HttpResponse(uint32_t status) :
+		HttpResponse(status, {}, "") {}
+	HttpResponse(uint32_t status, const std::string &body) :
+		HttpResponse(status, {}, body) {}
+	HttpResponse(uint32_t status, const KeyValueSet &header) :
+		HttpResponse(status, header, "") {}
+	HttpResponse(uint32_t status,
+		const KeyValueSet &header, const std::string &body) :
+		Status(status), Header(header), Body(body) {}
+
+	HttpResponse(const HttpResponse &) = default;
+	HttpResponse &operator=(const HttpResponse &) = default;
+	HttpResponse(HttpResponse &&) = default;
+	~HttpResponse() = default;
+
+};
+
+class WebPage {
+public:
+	WebPage() = default;
+	virtual ~WebPage() = default;
+
+	virtual void Get() = 0;
+};
 
 class HttpServer {
 public:
@@ -21,7 +64,7 @@ private:
 	// FD, メモリ のリミット
 	static const uint32_t MaxConn = 64;
 	// コネクションだけ確立して何もせずリソースを消費させる攻撃は NG
-	static const uint32_t TimeoutSec = 10;
+	static const uint32_t TimeoutSec = 60;
 	// 同一 IP アドレスからの接続数制限
 	static const uint32_t IpConnLimit = 16;
 	// スレッド数
@@ -29,7 +72,11 @@ private:
 
 	struct MHD_Daemon *m_daemon;
 
-	static int Answer(void *cls, struct MHD_Connection *connection,
+	HttpResponse ProcessRequest(struct MHD_Connection *connection,
+		const std::string &url, const std::string &method,
+		const std::string &version, const char *upload_data,
+		size_t *upload_data_size, void **con_cls) noexcept;
+	static int OnRequest(void *cls, struct MHD_Connection *connection,
 		const char *url, const char *method,
 		const char *version, const char *upload_data,
 		size_t *upload_data_size, void **con_cls) noexcept;
