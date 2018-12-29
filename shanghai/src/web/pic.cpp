4cd51e4541702e4178edf0a4ae420d275a4036b6
yappy <superunyo@yahoo.co.jp>
2018-12-29 16:35:53 +0900

    撮影時間を短くする
    暗くなってしまう可能性があるがやり直せばよい

--- shanghai/src/web/pic.cpp
+++ shanghai/src/web/pic.cpp
@@ -6,6 +6,7 @@ namespace shanghai {
 namespace web {
 
 namespace {
+	const char * const Timeout = "1";
 	const char * const ImgW = "1024";
 	const char * const ImgH = "768";
 	const char * const ImgTh = "160:120:100";
@@ -18,7 +19,7 @@ HttpResponse PicPage::Do(
 {
 	// 写真を stdout に出力する
 	Process p("/usr/bin/raspistill",
-		{"-o", "-", "-w", ImgW, "-h", ImgH, "-th", ImgTh});
+		{"-o", "-", "-t", Timeout, "-w", ImgW, "-h", ImgH, "-th", ImgTh});
 	int exitcode = p.WaitForExit(10);
 	if (exitcode != 0) {
 		return HttpResponse(500);
