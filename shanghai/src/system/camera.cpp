7ec4d31ab05f2d786b4058649a5698d9ba1fff52
yappy <superunyo@yahoo.co.jp>
2019-10-08 00:15:53 +0900

    写真撮影コマンド

--- shanghai/src/system/camera.cpp
+++ shanghai/src/system/camera.cpp
@@ -4,6 +4,8 @@
 #include "../util.h"
 #include "../exec.h"
 
+using mtx_guard = std::lock_guard<std::mutex>;
+
 namespace shanghai {
 namespace system {
 
@@ -16,7 +18,7 @@ Camera::Camera()
 	// std::filesystem が gcc 6.3.0 ではまだ experimental であり、
 	// コンパイラバージョン依存のソースとリンク設定(cmake)を書くのが煩雑なので
 	// mkdir -p を呼ぶ
-	Process p("/bin/mkdir", {"-p", m_picdir});
+	Process p{"/bin/mkdir", {"-p", m_picdir}};
 	int exitcode = p.WaitForExit();
 	if (exitcode != 0) {
 		throw FileError(p.GetErr());
@@ -31,12 +33,28 @@ Camera::Camera()
 	logger.Log(LogLevel::Info, "Initialize Camera OK");
 }
 
-std::string Take()
+void Camera::Take(const std::string &path,
+	uint32_t timeout_ms,
+	uint32_t w, uint32_t h,
+	uint32_t th_w, uint32_t th_h,
+	uint32_t th_quality)
 {
-	throw std::logic_error("not implemented");
+	mtx_guard lock{m_mtx};
+
+	// raspistill -o <path> -w <wsize> -h <hsize> -th <w>:<h>:<quality>
+	Process p{"/usr/bin/raspistill", {
+		"-o", path,
+		"-w", std::to_string(w),
+		"-h", std::to_string(h),
+		"-th", util::Format("{0}:{1}:{2}", {
+			std::to_string(th_w), std::to_string(th_h),
+			std::to_string(th_quality)
+		}),
+	}};
+	p.WaitForExit();
 }
 
-void RemoveOldFiles()
+void Camera::RemoveOldFiles()
 {
 	throw std::logic_error("not implemented");
 }
--- shanghai/src/system/camera.h
+++ shanghai/src/system/camera.h
@@ -1,3 +1,10 @@
+/*
+ * Command line full documentation
+ * https://www.raspberrypi.org/documentation/raspbian/applications/camera.md
+ * H/W S/W features
+ * https://www.raspberrypi.org/documentation/hardware/camera/
+ */
+
 #ifndef SHANGHAI_SYSTEM_CAMERA_H
 #define SHANGHAI_SYSTEM_CAMERA_H
 
@@ -9,10 +16,23 @@ namespace system {
 
 class Camera final {
 public:
+	static const uint32_t DEFAULT_TIMEOUT_MS = 5000;
+	// Camera Module v2
+	static const uint32_t DEFAULT_W = 3280;
+	static const uint32_t DEFAULT_H = 2464;
+	// thumbnail
+	static const uint32_t DEFAULT_THUMB_W = 160;
+	static const uint32_t DEFAULT_THUMB_H = 120;
+	static const uint32_t DEFAULT_THUMB_QUALITY = 100;
+
 	Camera();
 	~Camera() = default;
 
-	std::string Take();
+	void Take(const std::string &path,
+		uint32_t timeout_ms = DEFAULT_TIMEOUT_MS,
+		uint32_t w = DEFAULT_W, uint32_t h = DEFAULT_H,
+		uint32_t th_w = DEFAULT_THUMB_W, uint32_t th_h = DEFAULT_THUMB_H,
+		uint32_t th_quality = DEFAULT_THUMB_QUALITY);
 	void RemoveOldFiles();
 
 private:
