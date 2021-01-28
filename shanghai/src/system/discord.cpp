3a1d09561fc24ab7bd91edee78c95d65c661e9b8
yappy <superunyo@yahoo.co.jp>
2021-01-29 02:34:11 +0900

    sleepy-discord をリンク、初期化と終了

--- shanghai/src/system/discord.cpp
+++ shanghai/src/system/discord.cpp
@@ -2,9 +2,19 @@
 #include "../logger.h"
 #include "../util.h"
 #include "../config.h"
+#include <sleepy_discord/sleepy_discord.h>
 
 namespace shanghai {
 namespace system {
+
+class Discord::MyClient : public SleepyDiscord::DiscordClient {
+public:
+	using SleepyDiscord::DiscordClient::DiscordClient;
+	void onMessage(SleepyDiscord::Message message) override {
+		if (message.startsWith("whcg hello"))
+			sendMessage(message.channelID, "Hello " + message.author.username);
+	}
+};
 
 Discord::Discord()
 {
@@ -13,7 +23,36 @@ Discord::Discord()
 	bool enabled = config.GetBool({"Discord", "Enabled"});
 	std::string token = config.GetStr({"Discord", "Token"});
 
-	logger.Log(LogLevel::Info, "Initialize Discord OK");
+	if (enabled) {
+		m_client = std::make_unique<MyClient>(token, SleepyDiscord::USE_RUN_THREAD);
+		m_thread = std::thread([this]() {
+			try {
+				m_client->run();
+			}
+			catch (std::exception &e) {
+				logger.Log(LogLevel::Error, "Discord thread error: %s", e.what());
+			}
+		});
+		logger.Log(LogLevel::Info, "Initialize Discord OK");
+	}
+	else {
+		logger.Log(LogLevel::Info, "Initialize Discord OK (Disabled)");
+	}
+}
+
+Discord::~Discord()
+{
+	logger.Log(LogLevel::Info, "Finalize Discord...");
+	if (m_client != nullptr) {
+		logger.Log(LogLevel::Info, "Quit asio...");
+		logger.Flush();
+		m_client->quit();
+
+		logger.Log(LogLevel::Info, "Join discord thread...");
+		logger.Flush();
+		m_thread.join();
+	}
+	logger.Log(LogLevel::Info, "Finalize Discord OK");
 }
 
 }	// namespace system
--- shanghai/src/system/discord.h
+++ shanghai/src/system/discord.h
@@ -1,16 +1,21 @@
 #ifndef SHANGHAI_SYSTEM_DISCORD_H
 #define SHANGHAI_SYSTEM_DISCORD_H
 
+#include <memory>
+#include <thread>
+
 namespace shanghai {
 namespace system {
 
 class Discord final {
 public:
 	Discord();
-	~Discord() = default;
+	~Discord();
 
 private:
-
+	class MyClient;
+	std::thread m_thread;
+	std::unique_ptr<MyClient> m_client;
 };
 
 }	// namespace system
