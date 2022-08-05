51dd76eab1023f327866eaeb9ab7e27be9b1974a
yappy <superunyo@yahoo.co.jp>
2022-08-05 16:52:58 +0900

    Parse command line

--- rshanghai/src/main.rs
+++ rshanghai/src/main.rs
@@ -1,22 +1,35 @@
 extern crate daemonize;
+extern crate getopts;
 
+use getopts::Options;
+use std::env;
 use std::fs::File;
 use daemonize::Daemonize;
 
+const STDOUT_FILE: &str = "./stdout.txt";
 const STDERR_FILE: &str = "./stderr.txt";
 const PID_FILE: &str = "./rshanghai.pid";
 
-/// stderr をリダイレクトし、デーモン化する。
-/// stderr 用のファイルオープンに失敗したら exit(1) する。
+/// stdout, stderr をリダイレクトし、デーモン化する。
+/// ファイルオープンに失敗したら exit(1) する。
 /// デーモン化に失敗したら exit(1) する。
 /// 成功した場合は元の親プロセスは正常終了し、子プロセスが以降の処理を続行する。
 fn daemon() {
-    let stderr = File::create(STDERR_FILE);
-    if let Err(e) = stderr {
-        eprintln!("Open {} error: {}", STDERR_FILE, e);
-        std::process::exit(1);
-    }
-    let stderr = stderr.unwrap();
+    let stdout = match File::create(STDOUT_FILE) {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!("Open {} error: {}", STDOUT_FILE, e);
+            std::process::exit(1);
+        }
+    };
+
+    let stderr = match File::create(STDERR_FILE) {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!("Open {} error: {}", STDERR_FILE, e);
+            std::process::exit(1);
+        }
+    };
 
     let daemonize = Daemonize::new()
         .pid_file(PID_FILE)
@@ -24,7 +37,7 @@ fn daemon() {
         .working_directory(".")
         //.user("nobody")
         //.group("daemon")
-        //.stdout(stdout)
+        .stdout(stdout)
         .stderr(stderr);
 
     if let Err(e) = daemonize.start() {
@@ -33,6 +46,38 @@ fn daemon() {
     }
 }
 
+fn print_help(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
 fn main() {
-    daemon();
+    // コマンドライン引数のパース
+    let args: Vec<String> = env::args().collect();
+    let program = &args[0];
+
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "Print this help");
+    opts.optflag("d", "daemon", "Run as daemon");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(fail) => {
+            eprintln!("{}", fail);
+            std::process::exit(1);
+        }
+    };
+
+    // --help がある場合は出力して終了
+    if matches.opt_present("h") {
+        print_help(&program, opts);
+        std::process::exit(0);
+    }
+
+    if matches.opt_present("d") {
+        daemon();
+        println!("Daemon mode");
+    }
+    else {
+        println!("Not daemon mode");
+    }
 }
