f470fd8c4c186e118f7dcaef8820e4a01d66edde
yappy <superunyo@yahoo.co.jp>
2022-10-08 05:10:38 +0900

    key 配列で json tree を検索する

--- rshanghai/src/sys/config.rs
+++ rshanghai/src/sys/config.rs
@@ -37,3 +37,72 @@ pub fn init_and_load(def_json: &str, main_json: &str) -> Result<(), String>
 
     Ok(())
 }
+
+/// Object tree を keys: 文字列の配列 (スライス) で検索する。
+/// 見つからなかった場合、および
+/// 数値, 真偽値, 文字列, 配列 以外が見つかった場合は null を返す。
+/// 結果はコピーして返す。
+fn search(root: &serde_json::Value, keys: &[&str]) -> serde_json::Value {
+    let null_value = serde_json::Value::Null;
+
+    let mut value = root;
+    for key in keys.iter() {
+        value = match value.as_object() {
+            Some(map) => map.get(*key).unwrap_or(&null_value),
+            None => &null_value,
+        };
+    }
+
+    if value.is_number() || value.is_boolean() || value.is_string() || value.is_array() {
+        value.clone()
+    }
+    else {
+        serde_json::Value::Null
+    }
+}
+
+/// main -> def の順番で検索する。
+fn search_all(keys: &[&str]) -> serde_json::Value {
+    // rlock
+    let config = CONFIG
+        .get().unwrap()
+        .read().unwrap();
+
+    let value = search(&config.main, keys);
+    if !value.is_null() {
+        value
+    }
+    else {
+        search(&config.def, keys)
+    }
+}
+
+/// 文字列設定データを取得する。
+/// 見つからない場合や文字列でない場合は None を返す。
+pub fn get_string(keys: &[&str]) -> Option<String>
+{
+    search_all(keys).as_str().map(|s| s.into())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic() {
+        let main = r#"{"a": {"b": {"c": "main"}}}"#;
+        let def = r#"{"a": {"b": {"d": "default"}}}"#;
+
+        let result = init_and_load(def, main);
+        assert!(result.is_ok());
+
+        let v = get_string(&["a", "b", "c"]);
+        assert!(v.unwrap_or_default() == "main");
+        let v = get_string(&["a", "b", "C"]);
+        assert!(v.is_none());
+        let v = get_string(&["a", "b", "d"]);
+        assert!(v.unwrap_or_default() == "default");
+        let v = get_string(&["a", "b", "D"]);
+        assert!(v.is_none());
+    }
+}
