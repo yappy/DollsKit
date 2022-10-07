9721951a21b3441cb1dfb01975c5b40fe5196742
yappy <superunyo@yahoo.co.jp>
2022-10-08 06:43:55 +0900

    設定取得関数を追加
    serial_test についてメモ

--- rshanghai/README.md
+++ rshanghai/README.md
@@ -38,7 +38,40 @@ $ cargo clippy
 ```
 
 ## doc
---no-deps をつけないと、全ての依存先に対してドキュメント生成されて大変なことになる。
 ```
 $ cargo doc --no-deps
+```
+
+`--no-deps` をつけないと、全ての依存先に対してドキュメント生成されて大変なことになる。
+
+`--document-private-items` はバイナリクレートではデフォルトで有効になるので、
+public でない要素も出力される。
+
+`--open` をつけるとブラウザで開いてくれるそうだが、WSL では動かない。。
+
+# Tech Note
+## 並列テストが失敗する
+デフォルトで test attribute のついたテストは並列実行される。
+ファイルやグローバル変数等のグローバル状態を変更するテストは
+並列実行すると失敗する可能性がある。
+
+`cargo test` に `--test-threads=1` をつけるとシングルスレッド実行になるが、
+並列化可能なところまで直列化されてしまう。
+
+`mod test` 内にグローバル変数として Mutex を用意し直列化したいテストで
+ロックを取れば直列化できるが、assert 失敗で panic した場合に
+Mutex の PoisonError で他のテストを巻き込んで失敗してしまう。
+
+https://github.com/rust-lang/rust/issues/43155
+
+結論としては `serial_test` クレートを使うのが便利。
+```
+// これがついたテストは直列化される
+#[serial]
+// 引数をつけるとその名前のグループ内でのみ排他される
+#[serial(group)]
+// serial とは排他されるが、parallel 同士は同時に実行可能
+#[parallel]
+// こちらもグルーピング可能
+#[parallel(group)]
 ```
--- rshanghai/src/sys/config.rs
+++ rshanghai/src/sys/config.rs
@@ -1,8 +1,11 @@
 use once_cell::sync::OnceCell;
+use std::ops::RangeBounds;
 use std::sync::RwLock;
-use serde_json;
 use serde_json::json;
 
+/// グローバルに保持するデータ。
+/// ユーザによる設定と、見つからなかった場合に使うデフォルト値からなる。
+/// それぞれは Json object によるツリー構造。
 struct ConfigData {
     def:  serde_json::Value,
     main: serde_json::Value,
@@ -15,6 +18,9 @@ impl Default for ConfigData {
     }
 }
 
+/// 設定データ(グローバル変数)。
+/// 一応スレッドセーフとするが、初期化時に一度だけ書き換え、
+/// その後は read only アクセスしかしないので RwLock とする。
 static CONFIG: OnceCell<RwLock<ConfigData>> = OnceCell::new();
 
 /// 設定データを json 文字列からロードして設定データシステムを初期化する。
@@ -40,8 +46,8 @@ pub fn init_and_load(def_json: &str, main_json: &str) -> Result<(), String>
 
 /// Object tree を keys: 文字列の配列 (スライス) で検索する。
 /// 見つからなかった場合、および
-/// 数値, 真偽値, 文字列, 配列 以外が見つかった場合は null を返す。
-/// 結果はコピーして返す。
+/// 数値, 真偽値, 文字列, 配列 以外が見つかった場合は json - null を返す。
+/// グローバル構造内のデータを返す場合、コピーを返す。
 fn search(root: &serde_json::Value, keys: &[&str]) -> serde_json::Value {
     let null_value = serde_json::Value::Null;
 
@@ -77,19 +83,44 @@ fn search_all(keys: &[&str]) -> serde_json::Value {
     }
 }
 
+/// 真偽値設定データを取得する。
+/// 見つからない場合や真偽値でない場合は None を返す。
+pub fn get_boolean(keys: &[&str]) -> Option<bool> {
+    search_all(keys).as_bool()
+}
+
+/// 数値データを i64 で取得する。
+/// 見つからない場合、数値でない場合、範囲外の場合は None を返す。
+pub fn get_i64<R: RangeBounds<i64>>(keys: &[&str], range: R) -> Option<i64> {
+    search_all(keys).as_i64().and_then(|num| {
+        if range.contains(&num) { Some(num) }
+        else { None }
+    })
+}
+
+/// 数値データを u64 で取得する。
+/// 見つからない場合、数値でない場合、範囲外の場合は None を返す。
+pub fn get_u64<R: RangeBounds<u64>>(keys: &[&str], range: R) -> Option<u64> {
+    search_all(keys).as_u64().and_then(|num| {
+        if range.contains(&num) { Some(num) }
+        else { None }
+    })
+}
+
 /// 文字列設定データを取得する。
 /// 見つからない場合や文字列でない場合は None を返す。
-pub fn get_string(keys: &[&str]) -> Option<String>
-{
+pub fn get_string(keys: &[&str]) -> Option<String> {
     search_all(keys).as_str().map(|s| s.into())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serial_test::serial;
 
     #[test]
-    fn basic() {
+    #[serial(config)]
+    fn search() {
         let main = r#"{"a": {"b": {"c": "main"}}}"#;
         let def = r#"{"a": {"b": {"d": "default"}}}"#;
 
@@ -97,12 +128,32 @@ mod tests {
         assert!(result.is_ok());
 
         let v = get_string(&["a", "b", "c"]);
-        assert!(v.unwrap_or_default() == "main");
+        assert!(v.unwrap() == "main");
         let v = get_string(&["a", "b", "C"]);
         assert!(v.is_none());
         let v = get_string(&["a", "b", "d"]);
-        assert!(v.unwrap_or_default() == "default");
+        assert!(v.unwrap() == "default");
         let v = get_string(&["a", "b", "D"]);
+        assert!(v.is_none());
+    }
+
+    #[test]
+    #[serial(config)]
+    fn i64() {
+        let main = r#"{"i64": {"min": -9223372036854775808, "max": 9223372036854775807}}"#;
+        let def = "{}";
+
+        let result = init_and_load(def, main);
+        assert!(result.is_ok());
+
+        let v = get_i64(&["i64", "min"], ..);
+        assert_eq!(v.unwrap(), i64::MIN);
+        let v = get_i64(&["i64", "max"], ..);
+        assert_eq!(v.unwrap(), i64::MAX);
+
+        let v = get_i64(&["i64", "min"], i64::MIN+1..);
+        assert!(v.is_none());
+        let v = get_i64(&["i64", "max"], ..=i64::MAX-1);
         assert!(v.is_none());
     }
 }
