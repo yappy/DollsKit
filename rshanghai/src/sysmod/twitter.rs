5817fd7ed77ce08a3510944ece81a2733b607bf0
yappy <superunyo@yahoo.co.jp>
2022-10-21 03:02:55 +0900

    署名の生成まで (未テスト)

--- rshanghai/src/sysmod/twitter.rs
+++ rshanghai/src/sysmod/twitter.rs
@@ -1,10 +1,10 @@
-use rand::Rng;
-
 use crate::sys::config;
 use crate::sys::taskserver::Control;
+use crate::sys::net;
 use super::SystemModule;
 use std::collections::BTreeMap;
 use std::time::{SystemTime, UNIX_EPOCH};
+use rand::Rng;
 
 pub struct Twitter {
     enabled: bool,
@@ -117,7 +117,8 @@ fn create_oauth_field(consumer_key: &str, access_token: &str) -> KeyValue {
     }
     param.insert("oauth_nonce".into(), nonce_str);
 
-    // 署名は署名以外の o_auth フィールドに対しても行うので後で追加する
+    // 署名は署名以外の oauth_* フィールドに対しても行う
+    // 今はまだ不明なので後で追加する
     // param.emplace("oauth_signature", sha1(...));
 
     // oauth_signature_method, oauth_timestamp, oauth_token, oauth_version
@@ -134,10 +135,97 @@ fn create_oauth_field(consumer_key: &str, access_token: &str) -> KeyValue {
 }
 
 /// https://developer.twitter.com/en/docs/authentication/oauth-1-0a/creating-a-signature
+///
+/// oauth_param, query_param, body_param 内でキーの重複があると panic する。
 fn create_signature(
     http_method: &str, base_url: &str,
-    oauth_param: &KeyValue, query_param: &KeyValue,
+    oauth_param: &KeyValue, query_param: &KeyValue, body_param: &KeyValue,
     consumer_secret: &str, token_secret: &str)
+    -> String
 {
+    // "Collecting the request method and URL"
+    // Example:
+    // http_method = POST
+    // base_url = https://api.twitter.com/1.1/statuses/update.json
 
+    // "Collecting parameters"
+    // 以下の percent encode 前データを percent encode しながら 1つにまとめて
+    // キーの辞書順にソートする
+    // キーの重複は Twitter では認められていないのでシンプルに考えて OK
+    // * URL 末尾の query
+    // * request body
+    // * HTTP header の oauth_* パラメタ
+    //
+    // 1. Percent encode every key and value that will be signed.
+    // 2. Sort the list of parameters alphabetically [1] by encoded key [2].
+    // 3. For each key/value pair:
+    // 4. Append the encoded key to the output string.
+    // 5. Append the ‘=’ character to the output string.
+    // 6. Append the encoded value to the output string.
+    // 7. If there are more key/value pairs remaining, append a ‘&’ character to the output string.
+
+    // 1-2
+    let mut param = KeyValue::new();
+    let encode_add =
+    |param: &mut KeyValue, src: &KeyValue| {
+        for (k, v) in src.iter() {
+            let old = param.insert(
+                net::percent_encode(k),
+                net::percent_encode(v));
+            if old.is_some() {
+                panic!("duplicate key: {}", k);
+            }
+        }
+    };
+    encode_add(&mut param, oauth_param);
+    encode_add(&mut param, query_param);
+    encode_add(&mut param, body_param);
+
+    // 3-7
+    let mut parameter_string = "".to_string();
+    let mut is_first = true;
+    for (k, v) in param {
+        if is_first {
+            is_first = false;
+        }
+        else {
+            parameter_string.push_str(&k);
+            parameter_string.push('=');
+            parameter_string.push_str(&v);
+            parameter_string.push('&');
+        }
+    }
+
+    // "Creating the signature base string"
+    // "signature base string" by OAuth spec
+    // 署名対象となる文字列を生成する
+    // method, url, param を & でつなげるだけ
+    //
+    // 1. Convert the HTTP Method to uppercase and set the output string equal to this value.
+    // 2. Append the ‘&’ character to the output string.
+    // 3. Percent encode the URL and append it to the output string.
+    // 4. Append the ‘&’ character to the output string.
+    // 5. Percent encode the parameter string and append it to the output string.
+    let mut signature_base_string = "".to_string();
+    signature_base_string.push_str(&http_method.to_ascii_uppercase());
+    signature_base_string.push('&');
+    signature_base_string.push_str(&net::percent_encode(base_url));
+    signature_base_string.push('&');
+    signature_base_string.push_str(&net::percent_encode(&parameter_string));
+
+    // "Getting a signing key"
+    // 署名鍵は consumer_secret と token_secret をエスケープして & でつなぐだけ
+    let mut signing_key = "".to_string();
+    signing_key.push_str(consumer_secret);
+    signing_key.push('&');
+    signing_key.push_str(token_secret);
+
+    // "Calculating the signature"
+    // HMAC SHA1
+    let result = net::hmac_sha1(
+        signing_key.as_bytes(),
+        signature_base_string.as_bytes());
+
+    // base64 encode したものを署名として "oauth_signature" に設定する
+    base64::encode(result.into_bytes())
 }
