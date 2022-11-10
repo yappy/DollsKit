deb81ccda3e093aece1bd4fa5771928488499cbc
yappy <superunyo@yahoo.co.jp>
2022-11-11 03:09:30 +0900

    サーバ実行とキャンセルで select するとロックする問題を修正
    tokio::select! で選ばれず drop する(はず)と
    クロージャ内に move した値が drop されない
    原因不明なので select を使わない形に変える
    これかも？
    https://github.com/actix/actix-web/issues/1226

--- rshanghai/src/sysmod/httpserver.rs
+++ rshanghai/src/sysmod/httpserver.rs
@@ -1,10 +1,9 @@
 use super::SystemModule;
 use crate::sys::{config, taskserver::Control};
-use actix_web::{http::header::ContentType, HttpResponse, Responder};
+use actix_web::{dev::ServerHandle, http::header::ContentType, HttpResponse, Responder};
 use anyhow::{anyhow, Result};
 use log::info;
 use serde::{Deserialize, Serialize};
-use tokio::select;
 
 #[derive(Clone, Serialize, Deserialize)]
 struct HttpConfig {
@@ -12,15 +11,9 @@ struct HttpConfig {
     inaddr_any: bool,
 }
 
-#[actix_web::get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok()
-        .content_type(ContentType::plaintext())
-        .body("Hello world!\n")
-}
-
 pub struct HttpServer {
     config: HttpConfig,
+    handle: Option<ServerHandle>,
 }
 
 impl HttpServer {
@@ -31,38 +24,57 @@ impl HttpServer {
             config::get_object(&["http"]).map_or(Err(anyhow!("Config not found: http")), Ok)?;
         let config: HttpConfig = serde_json::from_value(jsobj)?;
 
-        Ok(Self { config })
+        Ok(Self {
+            config,
+            handle: None,
+        })
     }
 }
 
-async fn http_main_task(mut ctrl: Control) -> Result<()> {
-    loop {
-        let config = {
-            let http = ctrl.sysmods().http.lock().await;
-            http.config.clone()
-        };
-        let addr = if config.inaddr_any {
-            "0.0.0.0"
-        } else {
-            "127.0.0.1"
-        };
-        let server = actix_web::HttpServer::new(|| actix_web::App::new().service(hello))
-            .disable_signals()
-            .bind(("0.0.0.0", 8080))?
-            .run();
+async fn http_main_task(ctrl: Control) -> Result<()> {
+    let config = {
+        let http = ctrl.sysmods().http.lock().await;
+        http.config.clone()
+    };
+    let addr = if config.inaddr_any {
+        "0.0.0.0"
+    } else {
+        "127.0.0.1"
+    };
 
-        select! {
-            _ = server => {
-                info!("[http] server exit (reboot)");
-            },
-            _ = ctrl.cancel_rx().changed() => {
-                info!("[http] task cancel");
-                break;
-            },
-        }
-    }
+    // clone してクロージャ内に move する
+    let data = AppData::new(ctrl.clone());
+    // クロージャはワーカースレッドごとに複数回呼ばれる
+    let server = actix_web::HttpServer::new(move || {
+        actix_web::App::new().app_data(data.clone()).service(hello)
+    })
+    .disable_signals()
+    .bind(("0.0.0.0", 8080))?
+    .run();
+
+    // self.handle にサーバ停止用のハンドルを保存する
+    let mut http = ctrl.sysmods().http.lock().await;
+    http.handle = Some(server.handle());
+    drop(http);
+    // シャットダウンが来たらハンドルでサーバを停止するタスクを生成
+    ctrl.spawn_oneshot_task("http-exit", server_exit_task);
+
+    server.await?;
+    info!("[http] server exit");
 
     Ok(())
+}
+
+async fn server_exit_task(mut ctrl: Control) -> Result<()> {
+    let mut http = ctrl.sysmods().http.lock().await;
+    let handle = http.handle.take().unwrap();
+    drop(http);
+
+    let result = ctrl.cancel_rx().changed().await;
+
+    handle.stop(true).await;
+
+    Ok(result?)
 }
 
 impl SystemModule for HttpServer {
@@ -72,4 +84,13 @@ impl SystemModule for HttpServer {
             ctrl.spawn_oneshot_task("http", http_main_task);
         }
     }
+}
+
+type AppData = actix_web::web::Data<Control>;
+
+#[actix_web::get("/")]
+async fn hello(ctrl: AppData) -> impl Responder {
+    HttpResponse::Ok()
+        .content_type(ContentType::plaintext())
+        .body("Hello world!\n")
 }
