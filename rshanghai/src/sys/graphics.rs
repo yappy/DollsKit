13e028bb28287d363ffa7a9fb35cfb37f16e0511
yappy <superunyo@yahoo.co.jp>
2023-03-31 22:19:39 +0900

    文字配置によって時々起こる問題への対処
    先頭末尾文字の bounding_box が None の場合への対処
    範囲外ピクセルへのアクセス

--- rshanghai/src/sys/graphics.rs
+++ rshanghai/src/sys/graphics.rs
@@ -69,7 +69,8 @@ impl FontRenderer {
             }
         }
 
-        let mut image = DynamicImage::new_rgba8(width, glyphs_h * lines.len() as u32).to_rgba8();
+        let height = glyphs_h * lines.len() as u32;
+        let mut image = DynamicImage::new_rgba8(width, height).to_rgba8();
         for (_x, _y, pixel) in image.enumerate_pixels_mut() {
             *pixel = Rgba([0, 0, 0, 255 as u8]);
         }
@@ -81,6 +82,9 @@ impl FontRenderer {
                     glyph.draw(|x, y, v| {
                         let x = x + bounding_box.min.x as u32;
                         let y = y + bounding_box.min.y as u32;
+                        if x >= width || y >= height {
+                            return;
+                        }
                         let [r1, g1, b1, _a1] = image.get_pixel(x, y).0;
                         let [r2, g2, b2] = [255, 255, 255u8];
                         let r3 = (r1 as f32 * (1.0 - v) + r2 as f32 * v) as u8;
@@ -111,19 +115,25 @@ impl FontRenderer {
         y: f32,
         height: f32,
     ) -> (u32, Vec<PositionedGlyph>) {
+        if text.is_empty() {
+            return (0, Vec::new());
+        }
+
         let glyphs: Vec<_> = self
             .font
             .layout(text, scale, point(0.0, y + height))
             .collect();
 
-        let min_x = glyphs
-            .first()
-            .map(|g| g.pixel_bounding_box().unwrap().min.x)
-            .unwrap();
-        let max_x = glyphs
-            .last()
-            .map(|g| g.pixel_bounding_box().unwrap().max.x)
-            .unwrap();
+        let mut min_x = i32::MAX;
+        let mut max_x = i32::MIN;
+        for g in glyphs.iter() {
+            let (le, ri) = match g.pixel_bounding_box() {
+                Some(bb) => (bb.min.x, bb.max.x),
+                None => (g.position().x as i32, g.position().x as i32),
+            };
+            min_x = min_x.min(le);
+            max_x = max_x.max(ri);
+        }
 
         ((max_x - min_x) as u32, glyphs)
     }
@@ -144,12 +154,13 @@ mod tests {
         let ttf_bin = fs::read(fname)?;
 
         let r = FontRenderer::new(ttf_bin)?;
+        let text =
+"役割論理（Role logic）は、形式言語の一種であり、論理学やコンピュータ科学の分野で用いられる言語です。役割論理では、対象の役割に着目し、役割に関する命題を表現します。役割論理には、役割論理の基本的な概念である「役割」、「役割変数」、「役割制約」、「役割コンストラクタ」などがあります。
+役割論理では、対象の役割を表現するために、役割変数を用います。役割変数は、役割を表す変数であり、通常はRやSなどのアルファベットで表されます。役割制約は、役割変数が取りうる値に制限を加える制約のことです。たとえば、「人間は動物である」という役割制約は、「人間」の役割変数に対して、「動物」の値を許容することを意味します。
+役割コンストラクタは、複数の役割を組み合わせて新しい役割を構成するための方法です。たとえば、「親子関係」という役割は、「親」と「子」の役割を組み合わせて構成されます。役割コンストラクタには、合成（composition）や逆（inverse）などがあります。
+役割論理は、主に知識表現や推論に用いられます。例えば、役割論理を用いて、複数のエージェントの間で共有される知識を表現することができます。また、役割論理は、オントロジー言語の一種であるOWL（Web Ontology Language）の基盤としても用いられています。";
 
-        let png = r.draw_multiline_text(
-            "こんにちは。私はyappy家の管理人形です。\n\nくれぐれもよろしくお願いします。\n",
-            32,
-            320,
-        )?;
+        let png = r.draw_multiline_text(text, 16, 640)?;
 
         let fname = "font_test.png";
         println!("Write image to: {fname}");
