cc91cbc5c2b2738696a937328054e5b8b5ecb495
yappy <superunyo@yahoo.co.jp>
2024-04-13 02:49:36 +0900

    旧バージョン (C++) 削除

--- shanghai/src/task/task.h
+++
@@ -1,9 +0,0 @@
-#ifndef SHANGHAI_TASK_TASK_H
-#define SHANGHAI_TASK_TASK_H
-
-#include "taskconsume.h"
-#include "healthcheck.h"
-#include "ddns.h"
-#include "twittertask.h"
-
-#endif	// SHANGHAI_TASK_TASK_H
