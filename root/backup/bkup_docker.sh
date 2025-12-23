6f13fc15eddd7af4f68b4aecc6f46aeabd84f18f
yappy <superunyo@yahoo.co.jp>
2025-12-24 05:07:32 +0900

    プロジェクト内ボリュームの並列バックアップ

--- root/backup/bkup_docker.sh
+++ root/backup/bkup_docker.sh
@@ -6,16 +6,18 @@ echo Usage: "$0" PROJ VOLUMES...
 # Config (can be overriden by ENVVAR)
 BKUP_MP=${BKUP_MP:-"/mnt/bkup"}
 KEEP_COUNT=${KEEP_COUNT:-"30"}
-EXT=${EXT:-".tar.bz2"}
+EXT=${EXT:-"tar.bz2"}
 # ------------------------------------------------------------------------------
 
 #SELF_DIR=$(dirname "$(realpath "$0")")
 BKUP_ROOT="${BKUP_MP}/docker"
 PROJ=$1
 VOLUMES=( "${@:2}" )
+DSTDIR="${BKUP_ROOT}/${PROJ}"
 
 # check if mount point is available
 mountpoint "${BKUP_MP}"
+mkdir -p "${DSTDIR}"
 
 echo --------------------------------------------------------------------------------
 echo START
@@ -23,10 +25,11 @@ date -R
 echo --------------------------------------------------------------------------------
 
 for VOL in "${VOLUMES[@]}"; do
-    VOL_FULL="${PROJ}_${VOL}"
-    FILE="${VOL_FULL}_$(date +%Y%m%d).${EXT}"
-    docker run --rm -it -v "${VOL_FULL}:/mnt/vol" -v "${BKUP_ROOT}:/mnt/bkup" \
+    FILE="${VOL_FULL}_$(date +%Y%m%d%H%M ).${EXT}"
+    set -x
+    docker run --rm -v "${PROJ}_${VOL}:/mnt/vol" -v "${DSTDIR}:/mnt/bkup" \
         busybox tar caf "/mnt/bkup/${FILE}" -C /mnt/vol . &
+    set +x
 done
 wait
 
