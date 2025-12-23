1fd6b15ab08811e58b113d6ab38f3c6f183f433a
yappy <superunyo@yahoo.co.jp>
2025-12-24 02:24:15 +0900

    バックアップスクリプトのリファイン版を追加

---
+++ root/backup/bkup_all.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -eu
+
+SELF_DIR=$(dirname "$(realpath "$0")")
+SCRIPT_DIR=${SELF_DIR}/bkup/src
+
+# Can be overriden by ENVVAR
+BKUP_ROOT=${BKUP_ROOT:-"/mnt/bkup"}
+SRC_DIR=${SRC_DIR:-"/"}
+KEEP_COUNT=${KEEP_COUNT:-"10"}
+
+SYNC_DIR=${BKUP_ROOT}/sync
+ARCHIVE_DIR=${BKUP_ROOT}/archive
+
+# check if mount point is available
+mountpoint "${BKUP_ROOT}"
+
+echo --------------------------------------------------------------------------------
+echo START
+date -R
+echo --------------------------------------------------------------------------------
+
+python3 "${SCRIPT_DIR}/bkup.py" \
+sync \
+--src "${SRC_DIR}" \
+--dst "${SYNC_DIR}" \
+--exclude-from "${SELF_DIR}/bkup_exclude.txt" \
+--force
+
+python3 "${SCRIPT_DIR}/bkup.py" \
+archive \
+--src "${SYNC_DIR}" \
+--dst "${ARCHIVE_DIR}"
+
+python3 "${SCRIPT_DIR}/bkup.py" \
+clean \
+--dst "${ARCHIVE_DIR}" \
+--keep-count "${KEEP_COUNT}"
+
+echo --------------------------------------------------------------------------------
+echo END
+date -R
+echo --------------------------------------------------------------------------------
+echo ""
