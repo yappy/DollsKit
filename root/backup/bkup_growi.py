3e8758f939e9576a25dd174596c1fe74d432650e
yappy <superunyo@yahoo.co.jp>
2026-01-09 02:06:40 +0900

    圧縮アーカイブとクリーニング

--- root/backup/bkup_growi.py
+++ root/backup/bkup_growi.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import os
 import datetime
 import pathlib
@@ -9,15 +10,15 @@ import subprocess
 # ------------------------------------------------------------------------------
 # Config (can be overriden by ENVVAR)
 BKUP_MP = pathlib.Path(os.environ.get("BKUP_MP", "/mnt/bkup"))
-KEEP_COUNT = int(os.environ.get("KEEP_COUNT", "30"))
+KEEP_COUNT = os.environ.get("KEEP_COUNT", "30")
 PROJS = os.environ.get("PROJS", "growi-public growi-private").split()
 # ------------------------------------------------------------------------------
 
 SELF_DIR = pathlib.Path(os.path.dirname(__file__))
 SCRIPT_DIR = SELF_DIR / "bkup" / "src"
 BKUP_ROOT = BKUP_MP / "growi"
-BKUP_DUMP = BKUP_ROOT / "dump"
-BKUP_ARCHIVE = BKUP_ROOT / "archive"
+DUMP_DIR = BKUP_ROOT / "dump"
+ARCHIVE_DIR = BKUP_ROOT / "archive"
 # target GROWI service (DB)
 SERVICE = "mongo"
 
@@ -27,17 +28,46 @@ def exec(cmd: list[str]):
     subprocess.run(cmd, check=True)
 
 
-def dbdump():
-    pass
+# mongodump > /tmp/{proj}.archive (in the container)
+# docker compose cp to the host
+def dbdump(proj: str):
+    ar_path_cont = f"/tmp/{proj}.archive"
+    ar_path_host = str(DUMP_DIR / f"{proj}.archive")
+    exec([
+        "docker", "compose", "-p", proj, "exec", SERVICE,
+        "mongodump", "--quiet", f"--archive={ar_path_cont}"
+    ])
+    exec([
+        "docker", "compose", "-p", proj, "cp",
+        f"{SERVICE}:{ar_path_cont}", ar_path_host
+    ])
+
+
+def archive():
+    exec([
+        sys.executable, str(SCRIPT_DIR / "bkup.py"),
+        "archive",
+        "--src", str(DUMP_DIR),
+        "--dst", str(ARCHIVE_DIR),
+    ])
+
+
+def clean():
+    exec([
+        sys.executable, str(SCRIPT_DIR / "bkup.py"),
+        "clean",
+        "--dst", str(DUMP_DIR),
+        "--keep-count", KEEP_COUNT,
+    ])
 
 
 def main():
     # check if the mount point is available
     exec(["mountpoint", str(BKUP_MP)])
 
-    shutil.rmtree(BKUP_DUMP, ignore_errors=True)
-    BKUP_DUMP.mkdir(parents=True, exist_ok=True)
-    BKUP_ARCHIVE.mkdir(parents=True, exist_ok=True)
+    shutil.rmtree(DUMP_DIR, ignore_errors=True)
+    DUMP_DIR.mkdir(parents=True, exist_ok=True)
+    ARCHIVE_DIR.mkdir(parents=True, exist_ok=True)
 
     print("--------------------------------------------------------------------------------")
     print("START")
@@ -45,17 +75,9 @@ def main():
     print("--------------------------------------------------------------------------------")
 
     for proj in PROJS:
-        ar_path_cont = f"/tmp/{proj}.archive"
-        ar_path_host = str(BKUP_DUMP / f"{proj}.archive")
-        exec(["docker", "compose", "-p", proj, "exec", SERVICE,
-              "mongodump", "--quiet", f"--archive={ar_path_cont}"])
-        exec(["docker", "compose", "-p", proj, "cp",
-              f"{SERVICE}:{ar_path_cont}", ar_path_host]),
-
-        # python3 "${SCRIPT_DIR}/bkup.py" \
-        # clean \
-        # --dst "${ARCHIVE_DIR}" \
-        # --keep-count "${KEEP_COUNT}"
+        dbdump(proj)
+    archive()
+    clean()
 
     print("--------------------------------------------------------------------------------")
     print("END")
